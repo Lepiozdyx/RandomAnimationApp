9e29b38e0cbeb4bd97fd7481b0938085af802d61
Alex Zhulai <Lepiozdyx@gmail.com>
2023-05-02 21:08:05 -0300

    Add Model

---
+++ RandomAnimationApp/Models/Animation.swift
@@ -0,0 +1,25 @@
+//
+//  Animation.swift
+//  RandomAnimationApp
+//
+//  Created by Alex on 02.05.2023.
+//
+
+import Foundation
+
+struct Animation {
+    
+    let animation: String
+    let curve: String
+    let rotate: Double
+    
+    static func getRandomAnimation() -> Animation {
+        let animation = Animation(
+            animation: DataStore.shared.animations.randomElement() ?? "",
+            curve: DataStore.shared.curves.randomElement() ?? "",
+            rotate: Double.random(in: -5...5.0)
+        )
+        
+        return animation
+    }
+}
