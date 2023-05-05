f983c0b226ac82afee05943f7d83d3cf724d1d0b
Alex Zhulai <Lepiozdyx@gmail.com>
2023-05-05 12:27:56 -0300

    Change DataStore prop

--- RandomAnimationApp/Services/DataStore.swift
+++ RandomAnimationApp/Services/DataStore.swift
@@ -5,69 +5,14 @@
 //  Created by Alex on 02.05.2023.
 //
 
+import SpringAnimation
+
 final class DataStore {
+    
     static let shared = DataStore()
     
-    let animations = [
-        "pop",
-        "slideLeft",
-        "slideRight",
-        "slideDown",
-        "slideUp",
-        "squeezeLeft",
-        "squeezeRight",
-        "squeezeDown",
-        "squeezeUp",
-        "fadeIn",
-        "fadeOut",
-        "fadeOutIn",
-        "fadeInLeft",
-        "fadeInRight",
-        "fadeInDown",
-        "fadeInUp",
-        "zoomIn",
-        "zoomOut",
-        "fall",
-        "shake",
-        "flipX",
-        "flipY",
-        "morph",
-        "squeeze",
-        "flash",
-        "wobble",
-        "swing"
-    ]
-    let curves = [
-        "easeIn",
-        "easeOut",
-        "easeInOut",
-        "linear",
-        "spring",
-        "easeInSine",
-        "easeOutSine",
-        "easeInOutSine",
-        "easeInQuad",
-        "easeOutQuad",
-        "easeInOutQuad",
-        "easeInCubic",
-        "easeOutCubic",
-        "easeInOutCubic",
-        "easeInQuart",
-        "easeOutQuart",
-        "easeInOutQuart",
-        "easeInQuint",
-        "easeOutQuint",
-        "easeInOutQuint",
-        "easeInExpo",
-        "easeOutExpo",
-        "easeInOutExpo",
-        "easeInCirc",
-        "easeOutCirc",
-        "easeInOutCirc",
-        "easeInBack",
-        "easeOutBack",
-        "easeInOutBack"
-    ]
+    let animations = AnimationPreset.allCases
+    let curves = AnimationCurve.allCases
     
     private init() {}
 }
