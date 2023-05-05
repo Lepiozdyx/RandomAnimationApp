09b0741e873d66e2e4c6a741e0268d4ec35b38bd
Alex Zhulai <Lepiozdyx@gmail.com>
2023-05-05 12:28:25 -0300

    Change the Model

--- RandomAnimationApp/Models/Animation.swift
+++ RandomAnimationApp/Models/Animation.swift
@@ -5,18 +5,24 @@
 //  Created by Alex on 02.05.2023.
 //
 
-import Foundation
-
 struct Animation {
     
     let animation: String
     let curve: String
     let rotate: Double
     
-    static func getRandomAnimation() -> Animation {
+    var description: String {
+        """
+        preset: \(animation)
+        curve: \(curve)
+        rotate: \(String(format: "%.02f", rotate))
+        """
+    }
+    
+    static var randomAnimation: Animation {
         Animation(
-            animation: DataStore.shared.animations.randomElement() ?? "",
-            curve: DataStore.shared.curves.randomElement() ?? "",
+            animation: DataStore.shared.animations.randomElement()?.rawValue ?? "pop",
+            curve: DataStore.shared.curves.randomElement()?.rawValue ?? "easeIn",
             rotate: Double.random(in: -5...5.0)
         )
     }
