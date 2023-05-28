e5132c3b62c891fd63581bdda31032af6866154c
Alex Zhulai <Lepiozdyx@gmail.com>
2023-05-28 14:57:17 -0300

    Add more animation parameters

--- RandomAnimationApp/Models/Animation.swift
+++ RandomAnimationApp/Models/Animation.swift
@@ -10,12 +10,16 @@ struct Animation {
     let animation: String
     let curve: String
     let rotate: Double
+    let duration: Double
+    let delay: Double
     
     var description: String {
         """
         preset: \(animation)
         curve: \(curve)
         rotate: \(String(format: "%.02f", rotate))
+        duration: \(String(format: "%.02f", duration))
+        delay: \(String(format: "%.02f", delay))
         """
     }
     
@@ -23,7 +27,9 @@ struct Animation {
         Animation(
             animation: DataStore.shared.animations.randomElement()?.rawValue ?? "pop",
             curve: DataStore.shared.curves.randomElement()?.rawValue ?? "easeIn",
-            rotate: Double.random(in: -5...5.0)
+            rotate: Double.random(in: -5...5.0),
+            duration: Double.random(in: 0.8...1.5),
+            delay: 1
         )
     }
 }
